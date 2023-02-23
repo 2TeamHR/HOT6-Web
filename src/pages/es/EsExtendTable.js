7631274c66bfe5823483d8ff93381364152b964f
jyR <kfmxyqtycn1@gmail.com>
2023-02-23 11:08:59 +0900

    design: table border 수정

--- src/pages/es/EsExtendTable.js
+++ src/pages/es/EsExtendTable.js
@@ -88,15 +88,18 @@ import EsModal from "./EsModal";
         </TableRow>
   
         <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0, borderBottom: 0 }} colSpan={6}>
             <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box sx={{ margin: 1 }}>
+              <Box sx={{ margin: 1, }}>
                 <Typography variant="h6" gutterBottom component="div">
                   전자결재 미리보기
                 </Typography>
   
-                <HorizontalLabelPositionBelowStepper></HorizontalLabelPositionBelowStepper>
-                <Table size="small" aria-label="purchases"></Table>
+                <HorizontalLabelPositionBelowStepper>
+                </HorizontalLabelPositionBelowStepper>
+
+
+                
               </Box>
             </Collapse>
           </TableCell>
--- src/pages/es/EsModal.js
+++ src/pages/es/EsModal.js
@@ -4,11 +4,11 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Divider, Paper, Stack, styled } from '@mui/material';
+import { Divider, FormControl, Input, InputLabel, MenuItem, Paper, Select, Stack, styled } from '@mui/material';
 import dayjs from 'dayjs';
-import { Button,  Grid, TextField } from '@mui/material';
+import { Button, Grid, TextField, FilledInput } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -17,7 +17,7 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-  width: 200,
+  width: 250,
   height: 100
 }));
 
@@ -31,6 +31,16 @@ export default function EsModal() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const [leave, setLeave] = React.useState('');
+
+  const handleChange = (event) => {
+    setLeave(event.target.value);
+  };
+
+
+
+
   const [value, setValue] = React.useState(dayjs('2022-04-07'));
   return (
     <div>
@@ -40,66 +50,85 @@ export default function EsModal() {
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>전자결재</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            <Grid container direction="row">
-              <Grid item xs={8}>
-                  <h2>휴가 신청서</h2>
-                  <ul style={{ listStyle: "none" }}>
-                    <li>
-                      <label>결재선</label>
-                      <div>
-                        <Stack
-                          direction="row"
-                          divider={<Divider orientation="vertical" flexItem />}
-                          spacing={2}
-                        >
-                          <Item>기안자</Item>
-                          <Item>중간결재자</Item>
-                          <Item>최종결재자</Item>
-                        </Stack>
-                      </div>
-                    </li>
-                    <li><label>기안문서번호</label><input type="text" /></li>
-                    <li><label>기안일시</label><input type="date" name="" id="" /></li>
-                    <li><label>제목</label><input type="text" /></li>
-                    <li><label>내용</label><input type="text" name="" id="" /></li>
-                    <li><label>휴가종류</label><select name="" id="">
-                      <option value="1">연차휴가</option>
-                      <option value="2">출산휴가</option>
-                      <option value="3">배우자출산휴가</option>
-                      <option value="4">생리휴가</option>
-                      <option value="5">가족돌봄휴가</option>
-                    </select></li>
-                    <li><label htmlFor="">휴가시작</label>
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                          disableFuture
-                          openTo="day"
-                          views={['year', 'month', 'day']}
-                          value={value}
-                          onChange={(newValue) => {
-                            setValue(newValue);
-                          }}
-                          renderInput={(params) => <TextField size="small" {...params} />}
-                        />
-                      </LocalizationProvider></li>
-                    <li><label htmlFor="">휴가종료</label><LocalizationProvider dateAdapter={AdapterDayjs}>
-                      <DatePicker
-                        disableFuture
-                        openTo="day"
-                        views={['year', 'month', 'day']}
-                        value={value}
-                        onChange={(newValue) => {
-                          setValue(newValue);
-                        }}
-                        renderInput={(params) => <TextField size="small" {...params} />}
-                      />
-                    </LocalizationProvider></li>
-                    <li><label htmlFor="">첨부파일</label><input type="file" /></li> 
-                  </ul>
-              </Grid>
-            </Grid>
-          </DialogContentText>
+
+          <h2>휴가 신청서</h2>
+          <label>결재선</label>
+          <div>
+            <Stack
+              direction="row"
+              divider={<Divider orientation="vertical" flexItem />}
+              spacing={2}
+            >
+              <Item>기안자</Item>
+              <Item>중간결재자</Item>
+              <Item>최종결재자</Item>
+            </Stack>
+          </div>
+
+
+          <Grid container direction="row" columns={12}>
+            <Grid item xs={6}><label>기안문서번호</label></Grid><Grid item xs={6}><TextField id="standard-basic" variant="standard" /></Grid>
+            <Grid item xs={6}><label>기안일시</label></Grid><Grid item xs={6}><TextField id="standard-basic" variant="standard" /></Grid>
+            <Grid item xs={6}><label>제목</label></Grid><Grid item xs={6}><TextField id="standard-basic" variant="standard" /></Grid>
+            <Grid item xs={6}><label>내용</label></Grid><Grid item xs={6}><TextField
+              id="standard-multiline-static"
+
+              multiline
+              rows={8}
+
+            /></Grid>
+            <Grid item xs={6}><label>휴가종류</label></Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth >
+                <InputLabel id="demo-simple-select-label" margin='2'>휴가 종류</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={leave}
+                  label="Age"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>연차휴가</MenuItem>
+                  <MenuItem value={20}>출산휴가</MenuItem>
+                  <MenuItem value={30}>배우자출산휴가</MenuItem>
+                  <MenuItem value={40}>생리휴가</MenuItem>
+                  <MenuItem value={50}>가족돌봄휴가</MenuItem>
+                </Select>
+              </FormControl></Grid>
+
+            <Grid item xs={6}><label>휴가시작</label></Grid>
+            <Grid item xs={6}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  disableFuture
+                  openTo="day"
+                  views={['year', 'month', 'day']}
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => <TextField size="small" {...params} />}
+                />
+              </LocalizationProvider></Grid>
+            <Grid item xs={6}><label>휴가종료</label></Grid>
+            <Grid item xs={6}><LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                disableFuture
+                openTo="day"
+                views={['year', 'month', 'day']}
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField size="small" {...params} />}
+              />
+            </LocalizationProvider></Grid>
+            <Grid item xs={6}><label>첨부파일</label></Grid>
+            <Grid item xs={6}><Input /></Grid>
+
+          </Grid>
+
+
 
         </DialogContent>
         <DialogActions>
@@ -107,6 +136,6 @@ export default function EsModal() {
           <Button onClick={handleClose}>반려</Button>
         </DialogActions>
       </Dialog>
-    </div>
+    </div >
   );
 }
