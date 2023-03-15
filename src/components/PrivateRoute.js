a88e9ddbb6865f9074ef3786214e2631f69f8cd6
박준영 <duqrlqkrwnl@gmail.com>
2023-03-15 09:57:57 +0900

    feature: "로그인, "메인"
    페이지별 권환 수정중
    
    commit

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,16 +4,15 @@ import { Navigate } from 'react-router-dom';
 function PrivateRoute({ rules, component: Component }) {
 
     console.log('rules : ', rules);
-    console.log('rules : ', rules.auth);
 
   if (rules) {
 
     return Component;
-  } else {
-    
+  }
+
     alert("접근할 수 없는 페이지입니다.");
     return <Navigate to="/" />;
-  }
+  
 }
 
 export default PrivateRoute;
