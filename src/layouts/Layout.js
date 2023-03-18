76855b23d1ec39f9b3e88d33cb10f917012c4d84
jyR <kfmxyqtycn1@gmail.com>
2023-03-18 11:25:48 +0900

    레이아웃 코드 수정

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,16 +1,16 @@
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import SidebarV2 from '../pages/ea/SidebarV2';
 
 function Layout() {
 
     return (
         <>
-            {/* <Header /> */}
-            <Sidebar />
+            <Header/>
+            <Sidebar/>
             {/* <SidebarV2 /> */}
-            <Outlet />
+            <Outlet/>
         </>
     );
 }
