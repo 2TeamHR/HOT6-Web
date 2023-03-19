81feeaf71519a3a8354ec65007279de7bab08ad3
sangmoki <simbaesim@gmail.com>
2023-03-19 16:29:46 +0900

    commit

--- src/pages/salary/Salary_CheckNTable.js
+++ src/pages/salary/Salary_CheckNTable.js
@@ -1,122 +1,129 @@
 import { useEffect, useState } from "react";
-import { Pagination } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { callGetPagingSalaryAPI } from "../../apis/SalaryAPICalls";
+import { callGetPaymentSalaryAPI } from "../../apis/SalaryAPICalls";
+import AllSalaryModal from "./Salary_AllSalaryModal";
 
-function CheckNTable({ year, month, paymentsYn, currentPage, itemsPerPage, salaryData }) {
-  const dispatch = useDispatch();
-  const [showModal, setShowModal] = useState(false);
-  const [selectedSalaryCode, setSelectedSalaryCode] = useState("");
 
-  const salaryList = useSelector((state) => state.salaryReducer);
-  const member = useSelector((state) => state.salaryReducer);
-  const [memberInfo, setMemberInfo] = useState([]);
-  const [selectedData, setSelectedData] = useState([]);
+function CheckNTable() {
+    
 
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
+    const [showModal, setShowModal] = useState(false);
+    const [selectedSalaryCode, setSelectedSalaryCode] = useState("");
+    const [memberInfo, setMemberInfo] = useState({});
+    const [memberData, setMemberData] = useState([]);
+    const member = useSelector(state => state.salaryReducer);
+    const dispatch = useDispatch();
+    
+    function handleButtonClick(memberDetail) {
 
-  function handleButtonClick(memberDetail) {
-    setMemberInfo(memberDetail);
-    setShowModal(true);
-    setSelectedSalaryCode(memberDetail.salaryCode);
-  }
+        setMemberInfo(memberDetail);
+        setShowModal(true);
+        setSelectedSalaryCode(memberDetail.salaryCode);
+    }
 
-  useEffect(() => {
-    setSelectedData(salaryList.data?.content || []);
-  }, [salaryList.data]);
+    useEffect(() => {    
+            dispatch(callGetPaymentSalaryAPI({
+                year: "2023",
+                month: "02",
+                paymentsYn: "N"
+            })).then((data) => {
+                setMemberData(data);
+            });          
+    }
+    ,[memberInfo]
+    );
 
-  function handleCloseModal() {
-    setMemberInfo({});
-    setShowModal(false);
-  }
+    function handleCloseModal() {
+        setMemberInfo({});
+        setShowModal(false);
+    }
 
-  let memberList = "";
 
-  if (memberList !== undefined) {
-    memberList = member;
-  } else {
-    memberList.content = {
-      team: {
-        teamName: "",
-      },
-      rank: {
-        rankName: "",
-      },
-      memberName: "",
-      salary: {
-        afterSalary: 0,
-        paymentsYn: "",
-      },
-      salaryCode: "",
-    };
-  }
 
-  console.log("memberList====> ", memberList);
+    let memberList = [];
 
-  return (
-    <>
-      <div className="container-fluid">
-        <div className="table-area">
-          <table className="table table-hover">
-            <thead>
-              <tr style={{ backgroundColor: "#DDDDDD" }}>
-                <th>구분</th>
-                <th>조직</th>
-                <th>직급</th>
-                <th>이름</th>
-                <th>세후 지급액</th>
-                <th>지급여부</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {memberList.content.map((member) => (
-                <tr style={{ cursor: "pointer" }} key={member.salaryCode}>
-                  <td>{1}</td>
-                  <td>{member.team?.teamName}</td>
-                  <td>{member.rank?.rankName}</td>
-                  <td>{member.memberName}</td>
-                  <td>{member?.afterSalary}</td>
-                  <td>{member.paymentsYn}</td>
-                  <td style={{ width: 120 }}>
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => handleButtonClick(member)}
-                      onClose={handleCloseModal}
-                    >
-                      상세보기
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="d-flex justify-content-center mt-5">
-          <nav>
-            <ul className="pagination">
-              {[...Array(salaryList.data?.totalPages)].map((page, i) => (
-                <li
-                  key={i}
-                  className={`page-item ${
-                    currentPage === i + 1 ? "active" : ""
-                  }`}
-                >
-                  <button
-                    className="page-link"
-                    // onClick={() => onPageClick(i + 1)}
-                  >
-                    {i + 1}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </>
-  );
+    if(member !== undefined){
+        memberList.content = member;
+
+    } else {
+        memberList.content = {
+            team: {
+                teamName: ''
+            },
+            rank: {
+                rankName: ''
+            },
+            memberName: '',
+            salary: {
+                afterSalary: 0,
+                paymentsYn: ''
+            },
+            salaryCode: ''
+        };
+    }
+
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="table-area">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr style={{ "backgroundColor": "#DDDDDD" }}>
+                                <th>구분</th>
+                                <th>조직</th>
+                                <th>직급</th>
+                                <th>이름</th>
+                                <th>세후 지급액</th>
+                                <th>지급여부</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { memberList.length > 0 && memberList.map((memberDetail, index) => (
+                                <tr style={{ "cursor": "pointer" }} key={ memberDetail.content.salaryCode }>
+                                <td>{ index + 1 }</td>
+                                <td>{memberDetail.content.team?.teamName}</td>
+                                <td>{memberDetail.content.rank?.rankName}</td>
+                                <td>{memberDetail.content.memberName}</td>
+                                <td>{memberDetail.content.afterSalary}</td>
+                                <td>{memberDetail.content.paymentsYn}</td>
+                                <td style={{width:120}}>
+                                    <button 
+                                        className="btn btn-primary" 
+                                        onClick={() => handleButtonClick(memberDetail)}
+                                        onClose={handleCloseModal}
+                                    >
+                                        상세보기
+                                    </button>
+                                       
+                                    
+                                </td>
+                                </tr>
+                            ))
+                            }
+                            {showModal && <AllSalaryModal 
+                                onClose={handleCloseModal} 
+                                memberDetail={ memberInfo } 
+                                selectedSalaryCode= { selectedSalaryCode }    
+                                />}
+                        </tbody>
+                    </table>
+                </div>
+                <div align="center">
+                    <form name="search-form" autoComplete="off" style={{ "display": "inline-block" }}>
+                        <select id="searchCondition" name="searchCondition">
+                            <option value="teamName">조직</option>
+                            <option value="rankName">직급</option>
+                            <option value="memberName">이름</option>
+                        </select>
+                        <input type="search" id="searchValue" name="searchValue" placeholder="검색할 내용을 입력하세요." 
+                        />
+                        <input type="submit" id="searchList" className="btn btn-secondary" value="검색" />
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default CheckNTable;
