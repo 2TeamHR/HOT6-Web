ec883acdc47fc62656042dce6aea1716fb6a29c8
jaeyeongRoh <124746906+jaeyeongRoh@users.noreply.github.com>
2023-03-22 08:58:06 +0900

    Update EaDocumentInsertAPICalls.js

--- src/apis/EaDocumentInsertAPICalls.js
+++ src/apis/EaDocumentInsertAPICalls.js
@@ -24,4 +24,26 @@ export const callEaLeaveInsertAPI = ({form}) => {
         }
     };
 }
+export const callApprovalAPI = ({memberCode}) => {
+
+    let requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8888/ea/eaLeave/insert`;
+    console.log('eaRequestUrl', requestURL);
+    return async (dispatch, getState) => {
+        const result = await fetch(requestURL, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "*/*",
+            },
+            
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({type: PUT_APPROVAL_DOCUMENT, payload:result});
+            console.log(result.data);
+        }
+    };
+}
+
 
